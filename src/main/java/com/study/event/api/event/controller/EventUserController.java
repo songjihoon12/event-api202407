1c2d44c1c2ddffcc1fa1f3d555d1928548dcf191
songjihoon12 <jjang1702@naver.com>
2024-07-16 11:49:03 +0900

    1

--- src/main/java/com/study/event/api/event/controller/EventUserController.java
+++ src/main/java/com/study/event/api/event/controller/EventUserController.java
@@ -10,8 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.sql.SQLException;
+
 
 @RestController
 @RequestMapping("/auth")
