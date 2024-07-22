a44a1bd9c661d2fb2fca8548ac08a2cc247a6dc5
songjihoon12 <jjang1702@naver.com>
2024-07-22 12:15:56 +0900

    22

--- src/main/java/com/study/event/api/config/SecurityConfig.java
+++ src/main/java/com/study/event/api/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.DELETE,"/events/*").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.PUT,"/auth/promote").hasAuthority("COMMON")
                 //아래의 URL요청은 모두 허용
-                .antMatchers("/","/auth/**").permitAll()
+                .antMatchers("/","/auth/**", "/file/**").permitAll()
                 // 나머지 요청은 전부 인증 (로그인 ) 후 진행해라
                 .anyRequest().authenticated() //인가 설정 on
         ;
